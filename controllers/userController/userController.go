d9d13c905ab27ae6ff85b35dac5c0a06da73c8aa
renn27 <mrendialamsyah12@gmail.com>
2023-02-02 12:42:58 +0700

    menambah views

--- controllers/userController/userController.go
+++ controllers/userController/userController.go
@@ -1,13 +1,40 @@
 package usercontroller
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"html/template"
+
+	"github.com/renn27/go-crud/entities"
+)
 
 func Index(response http.ResponseWriter, request *http.Request) {
-
+	temp, err := template.ParseFiles("views/user/index.html")
+	if err != nil {
+		panic(err)
+	}
+	temp.Execute(response, nil)
 }
 
 func Add(response http.ResponseWriter, request *http.Request) {
+	if request.Method == http.MethodGet {
+		temp, err := template.ParseFiles("views/user/add.html")
+		if err != nil {
+			panic(err)
+		}
+		temp.Execute(response, nil)
+	} else if request.Method == http.MethodPost {
 
+		request.ParseForm()
+
+		var user entities.User
+		user.idUser = request.Form.Get("id_user")
+		user.nama = request.Form.Get("nama")
+		user.alamat = request.Form.Get("alamat")
+		user.no_telp = request.Form.Get("no_hp")
+
+		fmt.Println(user)
+	}
 }
 func Edit(response http.ResponseWriter, request *http.Request) {
 
--- main.go
+++ main.go
@@ -8,5 +8,11 @@ import (
 
 func main() {
 	http.HandleFunc("/", usercontroller.Index)
-	http.HandleFunc("/", usercontroller.Index)
+	http.HandleFunc("/user", usercontroller.Index)
+	http.HandleFunc("/user/index", usercontroller.Index)
+	http.HandleFunc("/user/add", usercontroller.Add)
+	http.HandleFunc("/user/edit", usercontroller.Edit)
+	http.HandleFunc("/user/delete", usercontroller.Delete)
+
+	http.ListenAndServe(":5000", nil)
 }
